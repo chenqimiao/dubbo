698b2f896f3d3aeb6e110feebf2f8e4d3ce33e7e
Qimiao Chen <chenqimiao1994@126.com>
2020-12-15 19:46:46 +0800

    Dubbo SPI TEST

--- dubbo-study/src/main/java/com/github/chenqimiao/spi/DefaultEchoService.java
+++ dubbo-study/src/main/java/com/github/chenqimiao/spi/DefaultEchoService.java
@@ -7,7 +7,7 @@ import org.apache.dubbo.common.extension.Adaptive;
  * @Author: Qimiao Chen
  * @Create: 2020-12-15 19:05
  **/
-@Adaptive
+// @Adaptive
 public class DefaultEchoService implements EchoService {
     @Override
     public String echo(String message) {
